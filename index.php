839f23293e7eea4b54602dd05c1b0e45a567d9d2
jugneyidk <jugneyidk@users.noreply.github.com>
2024-04-29 15:19:02 -0400

    Standard folders
    GymSys folders

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<php
-
-
-?php>
+<?php 
+$p = "landing"; 
+session_start();
+ if (!empty($_GET['p'])){
+   $p = $_GET['p'];
+  // if (!isset($_SESSION['id_usuario']) && ($p != "principal" && $p != "login" && $p != "consulta" && $p != "detallesdeuda")) {
+   //  header("Location: ?p=login");
+   //}
+ }
+ if(is_file("controlador/".$p.".php")){ 
+    require_once("controlador/".$p.".php");
+ }
+ else{
+   require_once("vista/404.php"); 
+ }
+?> 
