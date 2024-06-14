f2c6f793788248411f876d473d2dbb92841bacd8
diegonoss <diego14asf@gmail.com>
2024-06-14 00:46:56 -0400

    V1.11
    - Nueva plantilla modelo con encapsulado y bla bla bla

--- modelo/datos.php
+++ modelo/datos.php
@@ -1,15 +1,29 @@
-<?php 
-class datos{
+<?php
+class datos
+{
     // DATOS DE LA DB
-	private $ip = "localhost";
+    private $ip = "localhost";
     private $bd = "gymsysbd";
     private $usuario = "root";
     private $contrasena = "";
     // FUNCION PARA ESTABLECER CONEXION
-    function conecta(){
-        $pdo = new PDO("mysql:host=".$this->ip.";dbname=".$this->bd."",$this->usuario,$this->contrasena);
-         $pdo->exec("set names utf8");
-         return $pdo;
-         }        
-}      
+    public function conecta()
+    {
+        try {
+            $pdo = new PDO("mysql:host=" . $this->ip . ";dbname=" . $this->bd . "", $this->usuario, $this->contrasena);
+            $pdo->exec("set names utf8");
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+    public function validar_conexion($pdo)
+    {
+        if (!($pdo instanceof PDO)) {
+            throw new Exception($pdo, 1);
+        }
+
+    }
+}
 ?>
--- modelo/plantilla modelo.php
+++ modelo/plantilla modelo.php
@@ -2,15 +2,26 @@
 require_once ('modelo/datos.php');
 class xxxxx extends datos // reemplazar nombre con el nombre que deberia llevar dependiendo del modulo
 {
-    function incluir_XXXXX($id_atleta,$estado,$ultima_actualizacion) // reemplazar el nombre con que estan incluyendo
+    private $conexion, $id_atleta, $estado, $ultima_actualizacion; // todas las variables que se usaran que vienen del formulario (no tocar la conexion)
+    public function __construct()
+    {
+        $this->conexion = $this->conecta(); // inicia la conexion a la db
+    }
+    public function incluir_wada($id_atleta,$estado,$ultima_actualizacion){ // funcion publica que hace set a los atributos y llama a la funcion privada
+        $this->id_atleta = $id_atleta;
+        $this->estado = $estado;
+        $this->ultima_actualizacion = $ultima_actualizacion;
+        return $this->incluir();
+    }
+    public function listado_wada(){ // funcion publica que hace set a los atributos y llama a la funcion privada
+        return $this->listado();
+    }
+    private function incluir() // reemplazar el nombre con que estan incluyendo
     {
         try {
-            $db = new datos(); // se instancia la clase de la base de datos
-            $conexion = $db->conecta(); // se inicia la conexion de la db
-            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // se establecen los atributos para la conexion
-            $consulta = "INSERT FROM wada(id_atleta,estado,ultima_actualizacion) VALUES (:id_atleta,:estado,:ultima_actualizacion)"; // la consulta sql parametrizada
-            $valores = array(':id_atleta' => $id_atleta, ':estado' => $estado, ':ultima_actualizacion' => $ultima_actualizacion); // arreglo con los parametros que se enviaran en la consulta
-            $respuesta = $conexion->prepare($consulta); // se prepara LA CONSULTA
+            $consulta = "INSERT INTO wada(id_atleta,estado,ultima_actualizacion) VALUES (:id_atleta,:estado,:ultima_actualizacion)"; // la consulta sql parametrizada
+            $valores = array(':id_atleta' => $this->id_atleta, ':estado' => $this->estado, ':ultima_actualizacion' => $this->ultima_actualizacion); // arreglo con los parametros que se enviaran en la consulta
+            $respuesta = $this->conexion->prepare($consulta); // se prepara LA CONSULTA
             $respuesta->execute($valores); // se ejecuta la consulta PREPARADA y se pasa los VALORES a la funcion execute
             $resultado["ok"] = true; // se retorna OK true para saber en el js que la consulta salio bien
         } catch (Exception $e) {
@@ -19,16 +30,13 @@ class xxxxx extends datos // reemplazar nombre con el nombre que deberia llevar 
         }
         return $resultado; // se retorna el resultado
     }
-    function listado_XXXXX($formulario) // reemplazar el nombre con que estan incluyendo
+    private function listado() // reemplazar el nombre con que estan incluyendo
     {
         try {
-            $db = new datos(); // se instancia la clase de la base de datos
-            $conexion = $db->conecta(); // se inicia la conexion de la db
-            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // se establecen los atributos para la conexion
             $consulta = "SELECT * FROM `wada` WHERE id_atleta=:id_atleta"; // la consulta sql parametrizada, se quita la seleccion de fila si es para listar todos
-            $respuesta = $conexion->prepare($consulta); // se prepara LA CONSULTA
-            $valores = array(':id_atleta' => $formulario["id_atleta"]); // en este caso con valores si se va a seleccionar una sola fila
-            $respuesta->execute($valores); // se ejecuta la consulta PREPARADA y se pasa los VALORES en caso que tenga a la funcion execute
+            $respuesta = $this->conexion->prepare($consulta); // se prepara LA CONSULTA
+            // $valores = array(':id_atleta' => $formulario["id_atleta"]); // en este caso con valores si se va a seleccionar una sola fila
+            $respuesta->execute(); // se ejecuta la consulta PREPARADA y se pasa los VALORES en caso que tenga a la funcion execute
             $respuesta = $respuesta->fetchAll(PDO::FETCH_ASSOC); // se hace un FETCH ALL para traer todos los registros que coincidan, si deberia ser solo 1 se hace solo FETCH
             $resultado["ok"] = true; // se retorna OK true para saber en el js que la consulta salio bien
             $resultado["respuesta"] = $respuesta;
@@ -37,5 +45,15 @@ class xxxxx extends datos // reemplazar nombre con el nombre que deberia llevar 
             $resultado["mensaje"] = $e; // se retorna el mensaje de error
         }
         return $resultado; // se retorna el resultado
+    }
+
+    public function __get($propiedad) // no tocar, funcion magica para get
+    {
+        return $this->$propiedad;
+    }
+    public function __set($propiedad, $valor) // no tocar, funcion magica para set
+    {
+        $this->$propiedad = $valor;
+        return $this;
     }
 }
