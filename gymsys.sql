b4cb2492a7844edff5c0e62fe94f05acfcbd6e66
diegonoss <diego14asf@gmail.com>
2024-07-19 10:20:13 -0400

    V1.47.1
    DB

--- gymsys.sql
+++ gymsys.sql
@@ -1,14 +1,33 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-07-2024 a las 16:19:39
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de datos: `gymsys`
+--
 CREATE DATABASE IF NOT EXISTS `gymsys` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `gymsys`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asistencias`
+--
 
 CREATE TABLE `asistencias` (
   `id_atleta` varchar(10) NOT NULL,
@@ -16,6 +35,12 @@ CREATE TABLE `asistencias` (
   `fecha` date NOT NULL,
   `comentario` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `atleta`
+--
 
 CREATE TABLE `atleta` (
   `cedula` varchar(10) NOT NULL,
@@ -25,8 +50,11 @@ CREATE TABLE `atleta` (
   `altura` decimal(6,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `atleta` (`cedula`, `entrenador`, `tipo_atleta`, `peso`, `altura`) VALUES
-('28609560', '28609561', 0, 59.00, 169.00);
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bitacora`
+--
 
 CREATE TABLE `bitacora` (
   `id_accion` int(50) NOT NULL,
@@ -37,12 +65,24 @@ CREATE TABLE `bitacora` (
   `fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
 CREATE TABLE `categorias` (
   `id_categoria` int(5) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `peso_minimo` decimal(10,2) NOT NULL,
   `peso_maximo` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `competencia`
+--
 
 CREATE TABLE `competencia` (
   `id_competencia` int(50) NOT NULL,
@@ -55,13 +95,29 @@ CREATE TABLE `competencia` (
   `fecha_fin` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entrenador`
+--
+
 CREATE TABLE `entrenador` (
   `cedula` varchar(10) NOT NULL,
   `grado_instruccion` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `entrenador`
+--
+
 INSERT INTO `entrenador` (`cedula`, `grado_instruccion`) VALUES
 ('22222222', 'dsffdfsdf');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `marcas`
+--
 
 CREATE TABLE `marcas` (
   `id_marca` int(10) NOT NULL,
@@ -70,6 +126,12 @@ CREATE TABLE `marcas` (
   `envion` decimal(10,2) NOT NULL,
   `total` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mensualidades`
+--
 
 CREATE TABLE `mensualidades` (
   `id_mensualidad` int(50) NOT NULL,
@@ -80,13 +142,36 @@ CREATE TABLE `mensualidades` (
   `cobro` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `mensualidades` (`id_mensualidad`, `id_atleta`, `fecha`, `tipo`, `monto`, `cobro`) VALUES
-(2, '28609560', '2024-07-15', 0, 0.00, 0);
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `modulos`
+--
 
 CREATE TABLE `modulos` (
   `id_modulo` int(50) NOT NULL,
   `nombre` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `modulos`
+--
+
+INSERT INTO `modulos` (`id_modulo`, `nombre`) VALUES
+(1, 'entrenadores'),
+(2, 'atletas'),
+(3, 'rolespermisos'),
+(4, 'asistencias'),
+(5, 'eventos'),
+(6, 'mensualidad'),
+(7, 'wada'),
+(8, 'reportes');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `permisos`
+--
 
 CREATE TABLE `permisos` (
   `id_rol` int(50) NOT NULL,
@@ -97,12 +182,24 @@ CREATE TABLE `permisos` (
   `eliminar` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `representantes`
+--
+
 CREATE TABLE `representantes` (
   `cedula` varchar(10) NOT NULL,
   `id_atleta` varchar(10) NOT NULL,
   `nombre_completo` varchar(100) NOT NULL,
   `telefono` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `resultado_competencia`
+--
 
 CREATE TABLE `resultado_competencia` (
   `id_competencia` int(10) NOT NULL,
@@ -115,14 +212,30 @@ CREATE TABLE `resultado_competencia` (
   `total` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
 CREATE TABLE `roles` (
   `id_rol` int(50) NOT NULL,
   `nombre` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `roles`
+--
+
 INSERT INTO `roles` (`id_rol`, `nombre`) VALUES
 (0, 'atleta'),
 (1, 'entrenador');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `subs`
+--
 
 CREATE TABLE `subs` (
   `id_sub` int(5) NOT NULL,
@@ -131,10 +244,22 @@ CREATE TABLE `subs` (
   `edad_maxima` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_competencia`
+--
+
 CREATE TABLE `tipo_competencia` (
   `id_tipo_competencia` int(5) NOT NULL,
   `nombre` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
 
 CREATE TABLE `usuarios` (
   `cedula` varchar(10) NOT NULL,
@@ -148,9 +273,19 @@ CREATE TABLE `usuarios` (
   `correo_electronico` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
 INSERT INTO `usuarios` (`cedula`, `nombre`, `apellido`, `genero`, `fecha_nacimiento`, `lugar_nacimiento`, `estado_civil`, `telefono`, `correo_electronico`) VALUES
 ('22222222', 'jugneys', 'dfdfdf', 'Masculino', '2002-07-15', 'sdfdsfdfds', 'Soltero', '04245681343', 'dsfdsfd@gmail.com'),
 ('28609560', 'jugney', 'vargas', 'Masculino', '2002-07-15', 'dsdj', 'Soltero', '04245681343', 'KJSHJSHKJH@GMAIL.COM');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios_roles`
+--
 
 CREATE TABLE `usuarios_roles` (
   `id_usuario` varchar(10) NOT NULL,
@@ -159,9 +294,19 @@ CREATE TABLE `usuarios_roles` (
   `token` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Volcado de datos para la tabla `usuarios_roles`
+--
+
 INSERT INTO `usuarios_roles` (`id_usuario`, `id_rol`, `password`, `token`) VALUES
 ('22222222', 1, '$2y$10$syf4uVv4j1iML9whitgx2.ylZwVlWhUHrA7zFhvMyP0qqpzD6yNWO', '0'),
 ('28609560', 0, 'jugney28609560', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `wada`
+--
 
 CREATE TABLE `wada` (
   `id_atleta` varchar(10) NOT NULL,
@@ -171,143 +316,273 @@ CREATE TABLE `wada` (
   `estado` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Índices para tablas volcadas
+--
 
+--
+-- Indices de la tabla `asistencias`
+--
 ALTER TABLE `asistencias`
   ADD KEY `id_atleta` (`id_atleta`);
 
+--
+-- Indices de la tabla `atleta`
+--
 ALTER TABLE `atleta`
   ADD UNIQUE KEY `cedula` (`cedula`),
   ADD KEY `entrenador` (`entrenador`);
 
+--
+-- Indices de la tabla `bitacora`
+--
 ALTER TABLE `bitacora`
   ADD PRIMARY KEY (`id_accion`),
   ADD KEY `id_usuario` (`id_usuario`),
   ADD KEY `usuario_modificado` (`usuario_modificado`);
 
+--
+-- Indices de la tabla `categorias`
+--
 ALTER TABLE `categorias`
   ADD PRIMARY KEY (`id_categoria`);
 
+--
+-- Indices de la tabla `competencia`
+--
 ALTER TABLE `competencia`
   ADD PRIMARY KEY (`id_competencia`),
   ADD KEY `categoria` (`categoria`),
   ADD KEY `subs` (`subs`),
   ADD KEY `tipo_competicion` (`tipo_competicion`);
 
+--
+-- Indices de la tabla `entrenador`
+--
 ALTER TABLE `entrenador`
   ADD UNIQUE KEY `cedula` (`cedula`);
 
+--
+-- Indices de la tabla `marcas`
+--
 ALTER TABLE `marcas`
   ADD PRIMARY KEY (`id_marca`),
   ADD KEY `id_atleta` (`id_atleta`);
 
+--
+-- Indices de la tabla `mensualidades`
+--
 ALTER TABLE `mensualidades`
   ADD PRIMARY KEY (`id_mensualidad`),
   ADD KEY `id_atleta` (`id_atleta`);
 
+--
+-- Indices de la tabla `modulos`
+--
 ALTER TABLE `modulos`
   ADD PRIMARY KEY (`id_modulo`);
 
+--
+-- Indices de la tabla `permisos`
+--
 ALTER TABLE `permisos`
   ADD KEY `id_rol` (`id_rol`),
   ADD KEY `modulo` (`modulo`);
 
+--
+-- Indices de la tabla `representantes`
+--
 ALTER TABLE `representantes`
   ADD UNIQUE KEY `id_atleta` (`id_atleta`);
 
+--
+-- Indices de la tabla `resultado_competencia`
+--
 ALTER TABLE `resultado_competencia`
   ADD PRIMARY KEY (`id_competencia`),
   ADD KEY `id_atleta` (`id_atleta`);
 
+--
+-- Indices de la tabla `roles`
+--
 ALTER TABLE `roles`
   ADD PRIMARY KEY (`id_rol`);
 
+--
+-- Indices de la tabla `subs`
+--
 ALTER TABLE `subs`
   ADD PRIMARY KEY (`id_sub`);
 
+--
+-- Indices de la tabla `tipo_competencia`
+--
 ALTER TABLE `tipo_competencia`
   ADD PRIMARY KEY (`id_tipo_competencia`);
 
+--
+-- Indices de la tabla `usuarios`
+--
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`cedula`);
 
+--
+-- Indices de la tabla `usuarios_roles`
+--
 ALTER TABLE `usuarios_roles`
   ADD UNIQUE KEY `id_usuario` (`id_usuario`),
   ADD KEY `id_rol` (`id_rol`);
 
+--
+-- Indices de la tabla `wada`
+--
 ALTER TABLE `wada`
   ADD UNIQUE KEY `id_atleta` (`id_atleta`) USING BTREE;
 
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
 
+--
+-- AUTO_INCREMENT de la tabla `bitacora`
+--
 ALTER TABLE `bitacora`
   MODIFY `id_accion` int(50) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
 ALTER TABLE `categorias`
   MODIFY `id_categoria` int(5) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `competencia`
+--
 ALTER TABLE `competencia`
   MODIFY `id_competencia` int(50) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `marcas`
+--
 ALTER TABLE `marcas`
   MODIFY `id_marca` int(10) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `mensualidades`
+--
 ALTER TABLE `mensualidades`
   MODIFY `id_mensualidad` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
+--
+-- AUTO_INCREMENT de la tabla `modulos`
+--
 ALTER TABLE `modulos`
-  MODIFY `id_modulo` int(50) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_modulo` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
+--
+-- AUTO_INCREMENT de la tabla `resultado_competencia`
+--
 ALTER TABLE `resultado_competencia`
   MODIFY `id_competencia` int(10) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
 ALTER TABLE `roles`
-  MODIFY `id_rol` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_rol` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
+--
+-- AUTO_INCREMENT de la tabla `subs`
+--
 ALTER TABLE `subs`
   MODIFY `id_sub` int(5) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT de la tabla `tipo_competencia`
+--
 ALTER TABLE `tipo_competencia`
   MODIFY `id_tipo_competencia` int(5) NOT NULL AUTO_INCREMENT;
 
+--
+-- Restricciones para tablas volcadas
+--
 
+--
+-- Filtros para la tabla `asistencias`
+--
 ALTER TABLE `asistencias`
   ADD CONSTRAINT `asistencias_ibfk_1` FOREIGN KEY (`id_atleta`) REFERENCES `atleta` (`cedula`) ON UPDATE CASCADE;
 
+--
+-- Filtros para la tabla `atleta`
+--
 ALTER TABLE `atleta`
-  ADD CONSTRAINT `atleta_ibfk_1` FOREIGN KEY (`cedula`) REFERENCES `usuarios` (`cedula`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `atleta_ibfk_1` FOREIGN KEY (`cedula`) REFERENCES `usuarios` (`cedula`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `atleta_ibfk_2` FOREIGN KEY (`entrenador`) REFERENCES `entrenador` (`cedula`) ON UPDATE CASCADE;
 
+--
+-- Filtros para la tabla `bitacora`
+--
 ALTER TABLE `bitacora`
   ADD CONSTRAINT `bitacora_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`cedula`) ON UPDATE CASCADE,
   ADD CONSTRAINT `bitacora_ibfk_2` FOREIGN KEY (`usuario_modificado`) REFERENCES `usuarios` (`cedula`) ON UPDATE CASCADE;
 
+--
+-- Filtros para la tabla `competencia`
+--
 ALTER TABLE `competencia`
   ADD CONSTRAINT `competencia_ibfk_1` FOREIGN KEY (`tipo_competicion`) REFERENCES `tipo_competencia` (`id_tipo_competencia`) ON UPDATE CASCADE,
   ADD CONSTRAINT `competencia_ibfk_2` FOREIGN KEY (`categoria`) REFERENCES `categorias` (`id_categoria`) ON UPDATE CASCADE,
   ADD CONSTRAINT `competencia_ibfk_3` FOREIGN KEY (`subs`) REFERENCES `subs` (`id_sub`) ON UPDATE CASCADE;
 
+--
+-- Filtros para la tabla `entrenador`
+--
 ALTER TABLE `entrenador`
   ADD CONSTRAINT `entrenador_ibfk_1` FOREIGN KEY (`cedula`) REFERENCES `usuarios` (`cedula`) ON UPDATE CASCADE;
 
+--
+-- Filtros para la tabla `marcas`
+--
 ALTER TABLE `marcas`
   ADD CONSTRAINT `marcas_ibfk_1` FOREIGN KEY (`id_atleta`) REFERENCES `atleta` (`cedula`) ON UPDATE CASCADE;
 
+--
+-- Filtros para la tabla `mensualidades`
+--
 ALTER TABLE `mensualidades`
   ADD CONSTRAINT `mensualidades_ibfk_1` FOREIGN KEY (`id_atleta`) REFERENCES `atleta` (`cedula`) ON UPDATE CASCADE;
 
+--
+-- Filtros para la tabla `permisos`
+--
 ALTER TABLE `permisos`
   ADD CONSTRAINT `permisos_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`) ON UPDATE CASCADE,
   ADD CONSTRAINT `permisos_ibfk_2` FOREIGN KEY (`modulo`) REFERENCES `modulos` (`id_modulo`) ON UPDATE CASCADE;
 
+--
+-- Filtros para la tabla `representantes`
+--
 ALTER TABLE `representantes`
   ADD CONSTRAINT `representantes_ibfk_1` FOREIGN KEY (`id_atleta`) REFERENCES `atleta` (`cedula`) ON UPDATE CASCADE;
 
+--
+-- Filtros para la tabla `resultado_competencia`
+--
 ALTER TABLE `resultado_competencia`
   ADD CONSTRAINT `resultado_competencia_ibfk_1` FOREIGN KEY (`id_atleta`) REFERENCES `atleta` (`cedula`) ON UPDATE CASCADE,
   ADD CONSTRAINT `resultado_competencia_ibfk_2` FOREIGN KEY (`id_competencia`) REFERENCES `competencia` (`id_competencia`) ON UPDATE CASCADE;
 
+--
+-- Filtros para la tabla `usuarios_roles`
+--
 ALTER TABLE `usuarios_roles`
   ADD CONSTRAINT `usuarios_roles_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`) ON UPDATE CASCADE,
   ADD CONSTRAINT `usuarios_roles_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`cedula`) ON UPDATE CASCADE;
 
+--
+-- Filtros para la tabla `wada`
+--
 ALTER TABLE `wada`
   ADD CONSTRAINT `wada_ibfk_1` FOREIGN KEY (`id_atleta`) REFERENCES `atleta` (`cedula`) ON UPDATE CASCADE;
 COMMIT;
